037ebe56a7fce5689698732aa814d3a77dff6431
Osei37 <yamashita.osei@gmail.com>
2022-08-30 00:45:37 +0900

    refs #10 検索バーの構成部品を配置

--- frontend/src/modules/Form.jsx
+++ frontend/src/modules/Form.jsx
@@ -8,6 +8,10 @@ import MenuItem from '@material-ui/core/MenuItem';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import Grid from '@mui/material/Grid';
+import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import dayjs from 'dayjs';
 
 import axios from 'axios'
@@ -17,9 +21,11 @@ function Form(props) {
   const [category, setCategory] = useState("All");
   const [home, setHome] = useState(0);
   const [away, setAway] = useState(0);
-  const [day, setDay] = useState(dayjs('2022-08-18T21:11:54'));
+  const [day, setDay] = useState(dayjs("2016-09-22"));
   // const [week, setWeek] = useState("");
   const [setsu, setSetsu] = useState(0);
+  const [daySwitch, setDaySwitch] = useState(false);
+  const [setsuSwitch, setSetsuSwitch] = useState(false);
 
   const seasonValues = ["All", "2019-20", "2020-21", "2021-22"];
   const categoryValues = ["All", "B1", "B2", "B3"];
@@ -55,11 +61,23 @@ function Form(props) {
   const funSetSetsu = (e) => {
     setSetsu(() => e.target.value);
   }
+  const funSetDaySwitch = (e) => {
+    setDaySwitch(() => e.target.value);
+  }
+  const funSwitchDay = (e) => {
+    setDaySwitch(e.target.checked);
+  };
+  const funSwitchSetsu = (e) => {
+    setSetsuSwitch(e.target.checked);
+  };
 
   const funPost = () => {
     const params = new URLSearchParams();
-    params.append('season', season);
-    params.append('category', category);
+    params.append('season', season==="All" ? "2020-21" : season);
+    params.append('category', category==="All" ? "B1" : category);
+    params.append('home', home);
+    params.append('away', away);
+    
     axios.post('/apibscore', params)
       .then(function (res) {
         props.setDisplay({
@@ -82,98 +100,118 @@ function Form(props) {
 
   return (
     <>
-      <div style={{ justifyContent: "start" }}>
+      <Grid container style={{ justifyContent: "flex-start"}}>
+        <Grid item xs={2}>
+          <FormControl variant="standard" style={{ width: `${100 / numOfColumns}vw` }}>
+            <InputLabel>{gameDataList.season.column}</InputLabel>
+            <Select
+              label={gameDataList.season.column}
+              value={season}
+              onChange={funSetSeason}
+              >
+              {gameDataList.season.values.map((v) => (
+                <MenuItem value={v}>{v}</MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={2}>
+          <FormControl variant="standard" style={{ width: `${100 / numOfColumns}vw` }}>
+            <InputLabel>{gameDataList.category.column}</InputLabel>
+            <Select
+              label={gameDataList.category.column}
+              value={category}
+              onChange={funSetCategory}
+            >
+              {gameDataList.category.values.map((v) => (
+                <MenuItem value={v}>{v}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={2}>
+          <FormControl variant="standard" style={{ width: `${100 / numOfColumns}vw` }}>
+            <InputLabel>{gameDataList.home.column}</InputLabel>
+            <Select
+              label={gameDataList.home.column}
+              value={home}
+              onChange={funSetHome}
+            >
+              {gameDataList.home.values.map((v) => (
+                <MenuItem value={v}>{v}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={2}>
+          <FormControl variant="standard" style={{ width: `${100 / numOfColumns}vw` }}>
+            <InputLabel>{gameDataList.away.column}</InputLabel>
+            <Select
+              label={gameDataList.away.column}
+              value={away}
+              onChange={funSetAway}
+            >
+              {gameDataList.away.values.map((v) => (
+                <MenuItem value={v}>{v}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
 
-        <FormControl variant="standard" style={{ width: `${100 / numOfColumns}vw` }}>
-          <InputLabel>{gameDataList.season.column}</InputLabel>
-          <Select
-            label={gameDataList.season.column}
-            value={season}
-            onChange={funSetSeason}
-          >
-            {gameDataList.season.values.map((v) => (
-              <MenuItem value={v}>{v}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-
-        <FormControl variant="standard" style={{ width: `${100 / numOfColumns}vw` }}>
-          <InputLabel>{gameDataList.category.column}</InputLabel>
-          <Select
-            label={gameDataList.category.column}
-            value={category}
-            onChange={funSetCategory}
-          >
-            {gameDataList.category.values.map((v) => (
-              <MenuItem value={v}>{v}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-
-        <FormControl variant="standard" style={{ width: `${100 / numOfColumns}vw` }}>
-          <InputLabel>{gameDataList.home.column}</InputLabel>
-          <Select
-            label={gameDataList.home.column}
-            value={home}
-            onChange={funSetHome}
-          >
-            {gameDataList.home.values.map((v) => (
-              <MenuItem value={v}>{v}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-
-        <FormControl variant="standard" style={{ width: `${100 / numOfColumns}vw` }}>
-          <InputLabel>{gameDataList.away.column}</InputLabel>
-          <Select
-            label={gameDataList.away.column}
-            value={away}
-            onChange={funSetAway}
-          >
-            {gameDataList.away.values.map((v) => (
-              <MenuItem value={v}>{v}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-
-      </div>
-      <div style={{ justifycontent: "start" }}>
-
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DesktopDatePicker
-            label="Date desktop"
-            inputFormat="YYYY/MM/DD"
-            value={day}
-            onChange={funSetDay}
-            renderInput={(params) => <TextField {...params} style={{ width: `${100 / numOfColumns}vw` }}/>}
-          />
-        </LocalizationProvider>
-
-        {/* <FormControl variant="standard" style={{ width: `${100 / numOfColumns}vw` }}>
-          <InputLabel>{gameDataList.day.column}</InputLabel>
-          <Select label={gameDataList.day.column}>
-            {gameDataList.day.values.map((v) => (
-              <MenuItem value={v}>{v}</MenuItem>
-            ))}
-          </Select>
-        </FormControl> */}
-
+      <Grid container style={{ justifyContent: "flex-start" }}>
+        <Grid item xs={2}>
+          <FormGroup aria-label="position" row>
+            <FormControlLabel
+              value="top"
+              control={<Switch checked={daySwitch} onChange={funSwitchDay} color="primary" value={daySwitch} />}
+              label="Day"
+              labelPlacement="start"
+              />
+          </FormGroup>
+        </Grid>
+        <Grid item xs={2}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="Day"
+              inputFormat="YYYY/MM/DD"
+              value={day}
+              onChange={funSetDaySwitch}
+              emptyLabel="Select a date"
+              disabled={!daySwitch}
+              renderInput={(params) => <TextField {...params} style={{ width: `${100 / numOfColumns}vw` }}/>}
+            />
+          </LocalizationProvider>
+        </Grid>
+        <Grid item xs={2}>
+          <FormGroup aria-label="position" row>
+            <FormControlLabel
+              value="top"
+              control={<Switch checked={setsuSwitch} onChange={funSwitchSetsu} color="primary" value={daySwitch} />}
+              label="Setsu"
+              labelPlacement="start"
+              />
+          </FormGroup>
+        </Grid>
+        <Grid item xs={2}>
         <TextField
-          label="setsu"
+          label="Setsu"
           style={{ width: `${100 / numOfColumns}vw` }}
           value={setsu}
-          onChange={funSetSetsu}
+          onChange={funSwitchSetsu}
+          disabled={!setsuSwitch}
         />
-
-        <Button
-          onClick={funPost}
-        >
-          検索
-        </Button>
-
-      </div>
-
-
+        </Grid>
+        <Grid item xs={2}>
+          <Button
+            onClick={funPost}
+            size="large"
+            color="grey"
+          >
+            検索
+          </Button>
+        </Grid>
+      </Grid>
     </>
   )
 }
