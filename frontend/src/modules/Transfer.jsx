7a3b7343c6c4a6e1fda039e41e1280d618abb2a2
Osei37 <yamashita.osei@gmail.com>
2022-09-01 21:37:50 +0900

    refs #7 Headerの追加

---
+++ frontend/src/modules/Transfer.jsx
@@ -0,0 +1,13 @@
+import Header from './Header';
+
+function Transfer() {
+  return (
+    <>
+      <Header page="Transfer" />
+      <title>B.LEAGUE 移籍状況 (08/27)</title>
+      <h1>B.LEAGUE 移籍状況 (08/27)</h1>
+    </>
+  )
+  }
+  
+export default Transfer;
