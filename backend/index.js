16fa3241c54df72f097ed06f860ceba3d9c299d0
Osei37 <yamashita.osei@gmail.com>
2022-09-07 01:16:45 +0900

    refs #32 クエリをすべての条件で検索できるように修正

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,49 @@ app.get('/', (req, res) => {
 
 
 app.post("/apibscore", (req, res) => {
-  const sql = "SELECT * FROM info where season = ? and category = ?";
+  // 0 は全てのデータを取得する
+  let sql_list = []
+  let query_list = []
+  const season = req.body.season;
+  const category = req.body.category;
+  const home = req.body.home;
+  const away = req.body.away;
+  const day = req.body.day;
+  const setsu = req.body.setsu;
+
+  query_list.push("SELECT * FROM info");
+  if (season!="0") {
+    query_list.push("season = ?");
+    sql_list.push(season);
+  }
+  if (category!="0") {
+    query_list.push("category = ?");
+    sql_list.push(category);
+  }
+  if (home!="0") {
+    query_list.push("home = ?");
+    sql_list.push(home);
+  }
+  if (away!="0") {
+    query_list.push("away = ?");
+    sql_list.push(away);
+  }
+  if (day!="0") {
+    query_list.push("day = ?");
+    sql_list.push(day);
+  }
+  if (setsu!="0") {
+    query_list.push("setsu = ?");
+    sql_list.push(setsu);
+  }
+  if (query_list.length>1) {
+    query_list[0] += " WHERE " + query_list.slice(1).join(" AND ");
+  }
+  query_list[0] += " LIMIT 10";
+
   connection.query(
-    sql,
-    [req.body.season, req.body.category],
+    query_list[0],
+    sql_list,
     function(err, results, fields) {
       if(err) {
         console.log("接続終了(異常)");
