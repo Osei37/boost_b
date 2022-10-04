7e1b4471e913cedddfdf2d244c1eba692f058f64
Osei37 <yamashita.osei@gmail.com>
2022-10-04 18:04:31 +0900

    refs #50 トータルん表示を実装

--- frontend/src/modules/BoxScore.jsx
+++ frontend/src/modules/BoxScore.jsx
@@ -5,13 +5,15 @@ import Stats from './Stats';
 
 import axios from 'axios';
 
-function BoxScore(props) {
+function BoxScore() {
   const url = window.location.href.split('?').slice(-1)[0];
   const gameID = url.split('&').slice(0, 1)[0].split('=')[1];
   const home = url.split('&').slice(1, 2)[0].split('=')[1];
   const away = url.split('&').slice(2, 3)[0].split('=')[1];
   const [homeStats, setHomeStats] = useState(null);
+  const [homeTotalStats, setHomeTotalStats] = useState(null);
   const [awayStats, setAwayStats] = useState(null);
+  const [awayTotalStats, setAwayTotalStats] = useState(null);
 
   // ロード時に実行
   React.useEffect(() => {
@@ -23,20 +25,28 @@ function BoxScore(props) {
 
     axios.post('/getbscore', params)
       .then(function (res) {
+        console.log(res.data);
         setHomeStats(res.data.home);
+        setHomeTotalStats(res.data.hometotal[0]);
         setAwayStats(res.data.away);
+        setAwayTotalStats(res.data.awaytotal[0]);
       })
       .catch (function (error) {
         console.log("error", error);
       });
-  }, []);
+  }, [gameID, home, away]);
 
   return (
     <>
       <Header page="BoxScore"/>
       <h1>BoxScore</h1>
-      <Stats gameData={homeStats} />
-      <Stats gameData={awayStats} />
+      <div>
+        <Stats gameData={homeStats} totalData={homeTotalStats} />
+      </div>
+      {/* <h1></h1> */}
+      <div>
+        <Stats gameData={awayStats} totalData={awayTotalStats} />
+      </div>
       <Footer />
     </>
   )
